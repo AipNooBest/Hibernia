0df89dea5464612caafa0e5694ecada17d347a66
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-12-17 19:11:27 +0300

    Доработки кода и документации
    - Доработка режима разработки
    - Исправление документации
    - Корректировка ограничений

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/api/v1/login', (req, res) => {
 app.get('/api/v1/logout', (req, res) => {
     const session = req.header('cookie')?.split('=')[1];
     const pool = pools[session];
-    if (!pool || !session) return res.status(302).redirect('/login');
+    if (!pool || !session) return res.status(401).json({ error: 'Unauthorized' });
 
     pool.end();
     delete pools[session];
@@ -83,20 +83,17 @@ app.get('/api/v1/user/unpaid', checkSession, (req, res) => {
 app.get('/api/v1/visits', checkSession, (req, res) => {
     const pool = req.pool;
     let { start_year, start_month , end_year, end_month } = req.query;
-    if (!start_year || !start_month || !end_year || !end_month)
-        return res.status(400).json({ error: 'Start and end dates are required' });
-    if (!isInteger(start_year) || !isInteger(start_month) || !isInteger(end_year) || !isInteger(end_month))
-        return res.status(400).json({ error: 'Dates must be integers' });
+    if (!start_year || !start_month)
+        return res.status(400).json({ error: 'Start date is required' });
+    if (!end_year) end_year = new Date().getFullYear();
+    if (!end_month) end_month = new Date().getMonth();
+    if (!isValidYear(start_year) || !isValidMonth(start_month) || !isValidYear(end_year) || !isValidMonth(end_month))
+        return res.status(400).json({ error: 'Dates must be valid integers' });
     if (start_year > end_year || (start_year === end_year && start_month > end_month))
         return res.status(400).json({ error: 'Start date must be before end date' });
-    if (!end_year || !end_month) {
-        end_year = new Date().getFullYear();
-        end_month = new Date().getMonth();
-    }
 
     pool.query('SELECT * FROM get_visits($1, $2, $3, $4)', [start_year, start_month, end_year, end_month], (err, result) => {
         if (err) {
-            console.log(err);
             return res.status(500).json({ error: 'Error connecting to database' });
         }
         res.status(200).json(result.rows);
@@ -105,8 +102,13 @@ app.get('/api/v1/visits', checkSession, (req, res) => {
 
 app.get('/api/v1/groups/schedule', checkSession, (req, res) => {
     const pool = req.pool;
+    // Вообще, вроде как ученик должен уметь смотреть расписание только своей группы
+    // Сейчас же он вообще не может смотреть никакие расписания
     pool.query('SELECT * FROM groups_schedule', (err, result) => {
-        if (err) return res.status(500).json({ error: 'Error connecting to database' });
+        if (err) {
+            if (err.routine === 'aclcheck_error') return res.status(403).json({ error: 'Forbidden' });
+            return res.status(500).json({error: 'Error connecting to database'});
+        }
         res.status(200).json(result.rows);
     });
 })
@@ -123,7 +125,7 @@ app.get('/api/v1/groups/:id', checkSession, (req, res) => {
     const pool = req.pool;
     const { id } = req.params;
     if (!id) return res.status(400).json({ error: 'Group id is required' });
-    if (!isInteger(id)) return res.status(400).json({ error: 'Group id must be an integer' });
+    if (!isUInt(id)) return res.status(400).json({ error: 'Group id must be a valid integer' });
     pool.query('SELECT * FROM get_group_memberships($1)', [id], (err, result) => {
         if (err) return res.status(500).json({ error: 'Error connecting to database' });
         res.status(200).json(result.rows);
@@ -135,7 +137,16 @@ function generateSessionToken() {
 }
 
 function checkSession(req, res, next) {
-    if (process.env.NODE_ENV === 'development') return next();
+    if (process.env.NODE_ENV === 'development') {
+        req.pool = new pg.Pool({
+            user: process.env.TEST_USER,
+            password: process.env.TEST_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+        });
+        return next()
+    }
     const token = req.headers.cookie.split('=')[1];
     const pool = pools[token];
     if (!pool || !token) return res.status(401).json({ error: 'Unauthorized' });
@@ -143,8 +154,16 @@ function checkSession(req, res, next) {
     next();
 }
 
-function isInteger(value) {
-    return /^\d+$/.test(value);
+function isUInt(value) {
+    return /^\d+$/.test(value) && parseInt(value) > 0;
+}
+
+function isValidMonth(value) {
+    return /^\d+$/.test(value) && parseInt(value) > 0 && parseInt(value) < 13;
+}
+
+function isValidYear(value) {
+    return /^\d+$/.test(value) && parseInt(value) > 0;
 }
 
 function sanitizeString(str) {
