bc40ad390ad1b18d194794d3a7fc299071aa48c3
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-12-20 00:26:05 +0300

    Жалкие попытки добавить поддержку куки

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Expose-Headers", "Set-Cookie");
     next();
 });
 app.use('/api/v1', routes);
