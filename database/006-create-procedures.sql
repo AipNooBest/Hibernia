93565e6a7a1b04dfc8a96913d4b8d1cfd8d1b24d
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-12-19 17:01:42 +0300

    Небольшие правки на стороне БД
    - Изменён порядок выполнения скриптов, потому что из процедур вызываются функции, но не наоборот
    - Процедуре delete_pupil назначен SECURITY DEFINER

--- database/006-create-procedures.sql
+++ database/006-create-procedures.sql
@@ -7,15 +7,20 @@ BEGIN
     VALUES ($4, date_part('month', now()), date_part('year', now()), $1, $3, $2);
 END;
 $$ LANGUAGE plpgsql;
+REVOKE ALL ON FUNCTION add_pupil_to_accounting(int, money, money, int) FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION add_pupil_to_accounting(int, money, money, int) TO teacher;
 
 -- Процедура для удаления ученика из всех таблиц
 CREATE OR REPLACE PROCEDURE delete_pupil(id int)
+-- Процедура будет выполняться от имени владельца БД
+-- Делается это чтобы не давать отдельно учителю права на удаление ролей.
+SECURITY DEFINER
 AS $$
     DECLARE username text;
 BEGIN
     SELECT pupils.username INTO username FROM pupils WHERE pupils.id = $1;
     IF username IS NULL THEN
-        RAISE EXCEPTION 'Pupil with id % does not exist', $1;
+        RAISE EXCEPTION 'Ученик с ID % не существует', $1;
     END IF;
     DELETE FROM accounting WHERE pupil_id = $1;
     DELETE FROM concert_dance_lists WHERE pupil_id = $1;
@@ -23,14 +28,17 @@ BEGIN
     EXECUTE format('DROP USER %I', username);
 END;
 $$ LANGUAGE plpgsql;
+REVOKE ALL ON PROCEDURE delete_pupil(int) FROM PUBLIC;
+GRANT EXECUTE ON PROCEDURE delete_pupil(int) TO teacher;
 
 CREATE OR REPLACE PROCEDURE delete_pupil(username text)
+SECURITY DEFINER
 AS $$
     DECLARE user_id int;
 BEGIN
     SELECT pupils.id INTO user_id FROM pupils WHERE pupils.username = $1;
     IF user_id IS NULL THEN
-        RAISE EXCEPTION 'Pupil with username % does not exist', $1;
+        RAISE EXCEPTION 'Ученик с ником % не существует', $1;
     END IF;
     DELETE FROM accounting WHERE pupil_id = user_id;
     DELETE FROM concert_dance_lists WHERE pupil_id = user_id;
@@ -39,3 +47,5 @@ BEGIN
     EXECUTE format('DROP USER %I', username);
 END;
 $$ LANGUAGE plpgsql;
+REVOKE ALL ON PROCEDURE delete_pupil(text) FROM PUBLIC;
+GRANT EXECUTE ON PROCEDURE delete_pupil(text) TO teacher;
