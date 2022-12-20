0a9aaec67390f2254178a38df449a47c4cd6045e
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-12-20 22:30:19 +0300

    Правки разграничения ролей и удаление лишнего триггера

--- database/002-create-roles.sql
+++ database/002-create-roles.sql
@@ -1,8 +1,14 @@
 /* Разграничение ролей */
 
+CREATE ROLE admin;
 CREATE ROLE teacher;
 CREATE ROLE pupil;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO teacher;
+-- Разрешаем админу все действия, кроме дропа таблиц и БД
+-- Даже админы не должны уметь полностью ронять БД :^)
+-- Пускай этим занимается суперпользователь в лице какого-нибудь тех. админа
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO admin;
+GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public TO teacher;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO pupil;
+
 REVOKE SELECT ON group_management FROM pupil;
--- database/007-create-triggers.sql
+++ database/007-create-triggers.sql
@@ -1,7 +1,7 @@
 /* Создание триггеров */
 -- Триггер для удаления строк из таблицы accounting в случае, если прошло уже больше 12 месяцев
 CREATE OR REPLACE FUNCTION delete_old_accounting_row()
-RETURNS trigger AS $$
+RETURNS trigger SECURITY DEFINER AS $$
 DECLARE
     accounting_date date;
 BEGIN
@@ -17,26 +17,6 @@ CREATE TRIGGER delete_old_accounting_row
     BEFORE INSERT OR UPDATE ON accounting
     FOR EACH ROW
     EXECUTE PROCEDURE delete_old_accounting_row();
-
-/* Триггер для добавления имени пользователя в таблицу pupils при добавлении нового ученика
- * Плюс создание пользователя БД с именем, введённым при добавлении ученика
- * Если ник при добавлении ученика не введен, то он генерируется из ФИО ученика
- * Если ник уже занят, то к нему добавляется номер, начиная с 1                                     */
-CREATE OR REPLACE FUNCTION add_pupil_to_database()
-RETURNS trigger AS $$
-    DECLARE counter int := 1;
-BEGIN
-    IF NEW.username IS NULL THEN
-        NEW.username := transliterate(NEW.last_name || SUBSTR(NEW.first_name, 1, 3) || SUBSTR(NEW.second_name, 1, 3));
-    END IF;
-    WHILE EXISTS (SELECT * FROM pg_user WHERE usename = NEW.username) LOOP
-        NEW.username := NEW.username || counter;
-        counter := counter + 1;
-    END LOOP;
-    EXECUTE format('CREATE USER %I WITH PASSWORD %L IN ROLE pupil', NEW.username, 'SCRAM-SHA-256$4096:WhEvJT5jHZESufVLXmwp8g==$bZXK9Ckr9vvU1OUKKYS4eVK2ETgFjXvnqKrXdOOhTmw=:nbsEndgsQwzXq8LcBlvShX+5Kd97+ABIrmENHI+pHMs=');
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER add_pupil_to_database
     BEFORE INSERT ON pupils
